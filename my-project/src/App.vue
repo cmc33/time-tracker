f9f230bfc8bb84bbd46069d3d108969f710706ed
cmc33 <cmcguigan33@gmail.com>
2016-04-28 15:50:01 -0400

    add templates

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,4 +1,26 @@
 <template>
+  <div id="wrapper">
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <i class="glyphicon glyphicon-time"></i>
+          Vue Time Tracker
+        </a>
+        <ul class="nav navbar-nav">
+          <li><a v-link="'/home'">Home</a></li>
+          <li><a v-link="'/time-entries'">Time Entries</a></li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="col-sm-3">
+
+      </div>
+      <div class="col-sm-9">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
   <div id="app">
     <img class="logo" src="./assets/logo.png">
     <hello></hello>
@@ -22,43 +44,5 @@
   </div>
 </template>
 
-<script>
-import Hello from './components/Hello'
 
-export default {
-  components: {
-    Hello
-  }
-}
-</script>
 
-<style>
-html {
-  height: 100%;
-}
-
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
-
-#app {
-  color: #2c3e50;
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
-}
-
-#app a {
-  color: #42b983;
-  text-decoration: none;
-}
-
-.logo {
-  width: 100px;
-  height: 100px
-}
-</style>
