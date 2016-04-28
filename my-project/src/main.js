579a8366a21c62d29e31f470f00c2c05956156a4
cmc33 <cmcguigan33@gmail.com>
2016-04-28 16:03:18 -0400

    fixed routing issue wooo

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App'
-import Hello from './components/Hello.vue'
+import Home from './components/Home.vue'
 
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
@@ -12,7 +12,7 @@ const router = new VueRouter()
 
 router.map({
   '/hello': {
-    component: Hello
+    component: Home
   }
 })
 
