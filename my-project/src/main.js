3b4d1b7943ede1e3bebf264b4c6c65b532036f11
cmc33 <cmcguigan33@gmail.com>
2016-04-28 14:26:59 -0400

    set up basic routing

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,6 +1,28 @@
 import Vue from 'vue'
 import App from './App'
+import Hello from './components/Hello.vue'
 
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
+Vue.use(VueRouter)
+
+const router = new VueRouter()
+
+//Pointing routes to the components they should use
+router.map({
+  '/hello': {
+    component: Hello
+  }
+})
+
+//Any invalid route will redirect to home
+router.redirect({
+  '*': '/hello'
+})
+
+router.start(App, '#app')
 /* eslint-disable no-new */
 new Vue({
   el: 'body',
